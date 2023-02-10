b123becb8125c334afab30ec24702eb19f5faf79
Camilafabiani87 <54865542+Camilafabiani87@users.noreply.github.com>
2023-02-10 13:15:35 -0300

    Initial commit

---
+++ src/main/java/com/Challenge/QuintoImpacto/QuintoImpactoApplication.java
@@ -0,0 +1,13 @@
+package com.Challenge.QuintoImpacto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuintoImpactoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuintoImpactoApplication.class, args);
+	}
+
+}
